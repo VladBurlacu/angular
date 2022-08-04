0323074e5a91b9f0bd279a3fdc5fe0e6922380ab
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-08-04 11:58:34 +0200

    completed step 12

--- project-name/src/app/app.component.ts
+++ project-name/src/app/app.component.ts
@@ -1,1 +1,12 @@
+import { Component } from '@angular/core';
 import {Friend} from "./friend";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  languageArray:Array<string> = ['HTML', 'css','javascript','php', 'angular', 'python']
+  friendModel = new Friend('','','',0,'')
+}
