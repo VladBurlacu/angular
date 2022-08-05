65fe088adc624b7164494ebf9da87b106a179e4a
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-08-05 12:14:43 +0200

    working on step 15

--- project-name/src/app/add-friend.service.ts
+++ project-name/src/app/add-friend.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import {Friend} from "./friend";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddFriendService {
   private httpClient:HttpClient;
+  private url:string ="'http://localhost:9099/'";
+
   constructor() {
     this.httpClient = httpClient;
+  }
+  addFriend(data:Friend){
+    return this.httpClient.post(this.url, data)
   }
 }
--- project-name/src/app/app.component.ts
+++ project-name/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {Friend} from "./friend";
+import {AddFriendService} from "./add-friend.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -9,9 +11,14 @@ import {Friend} from "./friend";
 export class AppComponent {
   languageArray:Array<string> = ['HTML', 'css','javascript','php', 'angular', 'python']
   friendModel = new Friend('','','',0,'')
+  private addFriendService: AddFriendService;
 
   submitForm() {
     console.log(this.friendModel);
+  }
+
+  constructor() {
+    this.addFriendService = new AddFriendService()
   }
 }
 
